ad2c72a08efe690bed5f853a35cd1758219f1fa4
HamroNews <humagaincreativehouse@gmail.com>
2025-10-25 10:44:23 +0300

    Update day_one.dart

--- day_one.dart
+++ day_one.dart
@@ -1,4 +1,19 @@
+/*
+Welcome to dart lesson
+Today is our first day today we will understand what is dart
+dart is a programming language devloped by google.
+This is a multiplatform language where we can develp apps for windos,ios,android web ect.
+dart use .dart extention same like .java .js
+we will creat our first file like this main.dart main is file name and .dart is it's type like we make .jpeg .png .text files.
+lest creat a file main.dart and start progarmming.
+/main.dart(){
+  //this main() is a main function of dart from where we start the program everytime when we luch the program it looks from main() function and whaever we make the program it hase to be inside it.
+  // Remember this // is a way to comment in the program it can be seen for developer but while runing the progem it will be excluded from the complier.
+  //as every programming language display hello world lets make our very firs 
+*/
 
-void main(){
+
+
+  
   
 }
